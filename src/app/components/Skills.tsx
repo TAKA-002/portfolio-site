816e1ff3aa17e990f69d594ef0207de2d178249d
miura takayuki <6015abc@gmail.com>
2024-12-14 17:22:29 +0900

    Skillsのコンポーネントを基本実装

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 import Section from "./Utils/Section";
 import Heading from "./Utils/Heading";
+import HeadingLevel3 from "./Utils/HeadingLevel3";
+import { skillsData } from "./Skills/variables";
 
 const Skills = () => {
   return (
     <Section anchor="skills">
       <Heading text="Skills" />
+
+      <div className="my-20 grid grid-cols-2 gap-16">
+        {skillsData.map((item, index) => {
+          return (
+            <div key={index}>
+              <HeadingLevel3 text={item.name} />
+
+              <div className="flex flex-col gap-4">
+                {item.data.map((d, idx) => {
+                  return (
+                    <div className="flex item-center justify-between" key={idx}>
+                      <Paragraph text={d.name} />
+
+                      <div className="flex items-center gap-1">
+                        {[...Array(5)].map((_, index) => {
+                          return (
+                            <span
+                              key={index}
+                              className={`w-2 h-2 rounded-full ${
+                                index < d.score ? "bg-blue-500" : "bg-gray-200"
+                              }`}
+                            ></span>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </Section>
   );
+};
+
+export const Paragraph = ({ text }) => {
+  return <p className="text-gray-800">{text}</p>;
 };
 
 export default Skills;
---
+++ src/app/components/Skills/variables.tsx
@@ -0,0 +1,44 @@
+export const skillsData = [
+  {
+    name: "Frontend",
+    data: [
+      {
+        name: "HTML/CSS",
+        score: 5,
+      },
+      {
+        name: "JavaScript",
+        score: 3,
+      },
+      {
+        name: "TypeScript",
+        score: 1,
+      },
+      {
+        name: "React",
+        score: 2,
+      },
+      {
+        name: "Next.js",
+        score: 1,
+      },
+    ],
+  },
+  {
+    name: "Tools & Others",
+    data: [
+      {
+        name: "Git/GitHub",
+        score: 1,
+      },
+      {
+        name: "VS Code",
+        score: 1,
+      },
+      {
+        name: "Command Line",
+        score: 1,
+      },
+    ],
+  },
+];
