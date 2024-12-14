37869f609a3edf01b633ed196ba75dab3f540eb8
miura takayuki <6015abc@gmail.com>
2024-12-14 13:31:03 +0900

    jsonをfetchすることで表示させるように調整

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Section from "./Utils/Section";
 import Cards from "./Projects/Cards";
 
 const Projects = () => {
+  const [projectsData, setProjectsData] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjectsData() {
+      const res = await fetch("/data/projects.json");
+      if (res.ok) {
+        const { projects } = await res.json();
+        setProjectsData(projects);
+      } else throw new Error();
+    }
+
+    fetchProjectsData();
+  }, []);
+
   return (
     <Section anchor="projects">
       <h2 className="font-bold text-3xl">Projects</h2>
-      <Cards />
+      <Cards data={projectsData} />
     </Section>
   );
 };
--- src/app/components/Projects/Cards.tsx
+++ src/app/components/Projects/Cards.tsx
@@ -1,71 +1,70 @@
 import React from "react";
 import { Github, ExternalLink } from "lucide-react";
 
-const Cards = () => {
-  const data = [1, 2, 3, 4, 5, 6, 7, 8];
+const Cards = ({ data }) => {
   return (
     <div className="my-20">
       <ul className="grid grid-cols-projects gap-x-12 gap-y-16">
-        {data.map((i) => {
-          return (
-            <li className="flex flex-col gap-4" key={i}>
-              <figure className="w-full rounded-xl aspect-video overflow-hidden">
-                <img
-                  className="w-full h-full object-cover hover:scale-105 transition-transform dulation-500"
-                  src="https://picsum.photos/seed/picsum/500/300"
-                  alt=""
-                />
-              </figure>
+        {data.map(
+          ({ id, title, description, tags, image, sourceUrl, pageUrl }, i) => {
+            return (
+              <li id={`projects-${id}`} className="flex flex-col gap-4" key={i}>
+                <figure className="w-full rounded-xl aspect-video overflow-hidden">
+                  <img
+                    className="w-full h-full object-cover hover:scale-105 transition-transform dulation-500"
+                    src="https://picsum.photos/seed/picsum/500/300"
+                    alt=""
+                  />
+                </figure>
 
-              <div>
-                <h3 className="mb-4 text-xl font-semibold">Project One</h3>
-                <p
-                  className="mb-4 leading-normal"
-                  style={{
-                    fontSize: "min(3.7vw, 16px)",
-                  }}
-                >
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Sapiente, labore! Magni aperiam ab iure officiis!
-                </p>
-
-                <div className="mb-4 flex flex-row flex-wrap gap-2">
-                  <span className="px-4 rounded-xl bg-green-600">React</span>
-                  <span className="px-4 rounded-xl bg-green-600">Next.js</span>
-                  <span className="px-4 rounded-xl bg-green-600">Tailwind</span>
-                  <span className="px-4 rounded-xl bg-green-600">React</span>
-                  <span className="px-4 rounded-xl bg-green-600">Next.js</span>
-                  <span className="px-4 rounded-xl bg-green-600">Tailwind</span>
-                  <span className="px-4 rounded-xl bg-green-600">React</span>
-                  <span className="px-4 rounded-xl bg-green-600">Next.js</span>
-                  <span className="px-4 rounded-xl bg-green-600">Tailwind</span>
-                  <span className="px-4 rounded-xl bg-green-600">React</span>
-                  <span className="px-4 rounded-xl bg-green-600">Next.js</span>
-                  <span className="px-4 rounded-xl bg-green-600">Tailwind</span>
-                </div>
-
-                <div className="flex flex-row gap-4">
-                  <a
-                    className="flex flex-row gap-2"
-                    href=""
-                    aria-label="View source code for Project One"
+                <div>
+                  <h3 className="mb-4 text-xl font-semibold">{title}</h3>
+                  <p
+                    className="mb-4 leading-normal"
+                    style={{
+                      fontSize: "min(3.7vw, 16px)",
+                    }}
                   >
-                    <Github />
-                    Source
-                  </a>
-                  <a
-                    className="flex flex-row gap-2"
-                    href=""
-                    aria-label="Go to page for Project One"
-                  >
-                    <ExternalLink />
-                    Go to Page
-                  </a>
+                    {description}
+                  </p>
+
+                  <div className="mb-4 flex flex-row flex-wrap gap-2">
+                    {tags.map((item) => {
+                      return (
+                        <span className="px-4 rounded-xl bg-green-600">
+                          {item}
+                        </span>
+                      );
+                    })}
+                  </div>
+
+                  <div className="flex flex-row gap-4">
+                    {sourceUrl && (
+                      <a
+                        className="flex flex-row gap-2"
+                        href={sourceUrl}
+                        aria-label="View source code for Project One"
+                      >
+                        <Github />
+                        Source
+                      </a>
+                    )}
+                    {pageUrl && (
+                      <a
+                        className="flex flex-row gap-2"
+                        href={pageUrl}
+                        aria-label="Go to page for Project One"
+                      >
+                        <ExternalLink />
+                        Go to Page
+                      </a>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          }
+        )}
       </ul>
     </div>
   );
