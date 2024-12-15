499844c603fa031b1aa85a983f56facc79c1f0a6
miura takayuki <6015abc@gmail.com>
2024-12-16 02:25:36 +0900

    ダイナミックルーティングのidと、そのページの情報を一括で管理するための変数を作成

---
+++ src/app/projects/variables.ts
@@ -0,0 +1,28 @@
+export type PageItem = {
+  id: string;
+  title: string;
+  // 他のプロパティも追加可能
+}
+
+export const pageData: PageItem[] = [
+  {
+    id: "1",
+    title: "aaaaaaaaaa"
+  },
+  {
+    id: "2",
+    title: "aaaaaaaaaa"
+  },
+  {
+    id: "3",
+    title: "aaaaaaaaaa"
+  },
+  {
+    id: "4",
+    title: "aaaaaaaaaa"
+  },
+  {
+    id: "5",
+    title: "aaaaaaaaaa"
+  }
+]
