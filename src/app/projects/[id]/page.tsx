e5aee24b972c7babfdb8e8fcd1fa2dec323ee333
miura takayuki <6015abc@gmail.com>
2024-12-16 02:26:23 +0900

    ダイナミックルーティングのidに付随した情報を使用してページを構成するためのコンポーネント整備を実施

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,13 +1,13 @@
+import { pageData } from "../variables";
+
 // 静的サイト生成（SSG）+ ダイナミックルーティングの場合、明示しないといけない。
 export const dynamic = "force-static";
 
 // 事前にIDのリストをわたしておく。そのとき、promiseを返す関数generateStaticParamsで準備する。
 export async function generateStaticParams() {
   // 例: プロジェクトIDのリストを返す
-  const ids = ["1", "2", "3", "4", "5"];
-
-  return ids.map((id) => ({
-    id: id,
+  return pageData.map((item) => ({
+    id: item.id,
   }));
 }
 
@@ -19,9 +19,17 @@ export default async function ProjectPage({ params }: ProjectParams) {
   // paramsはpromiseで扱われるため、そのpromiseを解決する必要がある。
   const { id } = await Promise.resolve(params);
 
+  // pageDataから、paramsのidと同じデータをまとめているオブジェクトを取得
+  const pageItem = pageData.find((item) => item.id === id);
+  if (!pageItem) return;
+
+  // オブジェクトから情報を取得
+  const { title } = pageItem;
+
   return (
     <div>
       <h1 className="h-[1000px] bg-black">Project {id}</h1>
+      <h2>{title}</h2>
     </div>
   );
 }
