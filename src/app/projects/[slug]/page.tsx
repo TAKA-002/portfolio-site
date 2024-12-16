2168658c6927708369233c98c57e8339a919aa6a
miura takayuki <6015abc@gmail.com>
2024-12-17 01:32:36 +0900

    metaデータをvariablesの変数から取得するようにgenerateMetadataメソッドを使用

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { pageData } from "../variables";
 
 // 静的サイト生成（SSG）+ ダイナミックルーティングの場合、明示しないといけない。
 export const dynamic = "force-static";
+
+type ProjectParams = {
+  params: Promise<{ slug: string }>;
+};
 
 // 事前にIDのリストをわたしておく。そのとき、promiseを返す関数generateStaticParamsで準備する。
 export async function generateStaticParams() {
@@ -11,19 +17,38 @@ export async function generateStaticParams() {
   }));
 }
 
-type ProjectParams = {
-  params: Promise<{ slug: string }>;
-};
+export async function generateMetadata({
+  params,
+}: ProjectParams): Promise<Metadata> {
+  const { slug } = await params;
+  const projectData = pageData.find((item) => item.slug === slug);
+
+  // pageDataにslugの文字列が存在しない場合404ページ用titleを設定
+  if (!projectData) {
+    return {
+      title: "Not Found | Takayuki Miura PortFolio",
+    };
+  }
+
+  return {
+    title: `${projectData?.title} | Takayuki Miura PortFolio`,
+    openGraph: {
+      title: `${projectData?.title} | Takayuki Miura PortFolio`,
+      type: "article",
+    },
+  };
+}
 
 export default async function ProjectPage({ params }: ProjectParams) {
   // paramsはpromiseで扱われるため、そのpromiseを解決する必要がある。
   // 型定義の段階で、promiseとして定義しているので、Promise.resolveでPromise変換が不要。
-  const resolvedParams = await params;
-  const { slug } = resolvedParams;
+  const { slug } = await params;
 
   // pageDataから、paramsのidと同じデータをまとめているオブジェクトを取得
   const pageItem = pageData.find((item) => item.slug === slug);
-  if (!pageItem) return;
+  if (!pageItem) {
+    notFound();
+  }
 
   // オブジェクトから情報を取得
   const { title } = pageItem;
--- src/app/projects/variables.ts
+++ src/app/projects/variables.ts
@@ -2,33 +2,32 @@ export type PageItem = {
   id: string;
   slug: string,
   title: string;
-  // 他のプロパティも追加可能
 }
 
 export const pageData: PageItem[] = [
   {
     id: "1",
     slug: "bdc-lp",
-    title: "BDC ピラティス LPページコーディング | Takayuki Miura PortFolio"
+    title: "BDC ピラティス LPページコーディング"
   },
   {
     id: "2",
     slug: "text-manipulator",
-    title: "Text Manipulator App | Takayuki Miura PortFolio"
+    title: "Text Manipulator App"
   },
   {
     id: "3",
     slug: "image-creator",
-    title: "image-creator Web App | Takayuki Miura PortFolio"
+    title: "image-creator Web App"
   },
   {
     id: "4",
     slug: "senkyo-web",
-    title: "選挙WEB 各種ページ制作・運用 | Takayuki Miura PortFolio"
+    title: "選挙WEB 各種ページ制作・運用"
   },
   {
     id: "5",
     slug: "worldcup-2022",
-    title: "サッカーワールドカップ2022 WebAPI データ処理 | Takayuki Miura PortFolio"
+    title: "サッカーワールドカップ2022 WebAPI データ処理"
   }
 ]
