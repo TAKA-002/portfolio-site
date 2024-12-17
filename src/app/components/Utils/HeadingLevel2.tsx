65cb8ef0dfa4a093717cae369770af184a78416d
miura takayuki <6015abc@gmail.com>
2024-12-18 02:04:54 +0900

    h2のコンポーネントを拡張

--- src/app/components/Utils/HeadingLevel2.tsx
+++ src/app/components/Utils/HeadingLevel2.tsx
@@ -2,8 +2,13 @@ import React from "react";
 
 type HeadingLevel2Props = {
   text: string;
+  useProject: boolean;
 };
-const HeadingLevel2 = ({ text }: HeadingLevel2Props) => {
+const HeadingLevel2 = ({ text, useProject }: HeadingLevel2Props) => {
+  if (useProject) {
+    return <h2 className="font-bold text-2xl">{text}</h2>;
+  }
+
   return <h2 className="font-bold text-3xl">{text}</h2>;
 };
 
