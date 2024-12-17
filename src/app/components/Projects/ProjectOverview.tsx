6b86c519d0d0588b7e8b2d897b7222dd78de9e44
miura takayuki <6015abc@gmail.com>
2024-12-18 03:25:31 +0900

    型定義を追加

--- src/app/components/Projects/ProjectOverview.tsx
+++ src/app/components/Projects/ProjectOverview.tsx
@@ -3,11 +3,30 @@ import { Github, ExternalLink } from "lucide-react";
 import HeadingLevel2 from "../Utils/HeadingLevel2";
 import ProjectTags from "./ProjectTags";
 
-const InfomationTitle = ({ text }) => {
+interface InformationTitleProps {
+  text: string;
+}
+
+const InformationTitle = ({ text }: InformationTitleProps) => {
   return <span className="text-sm text-gray-400 font-semibold">{text}</span>;
 };
 
-const ProjectOverview = ({ overview, id }) => {
+interface ProjectOverviewProps {
+  overview: {
+    text: string;
+    tags: string[];
+    period: string;
+    role: string;
+    team: string;
+    links: {
+      sourceUrl: string;
+      pageUrl: string;
+    };
+  };
+  id: string;
+}
+
+const ProjectOverview = ({ overview, id }: ProjectOverviewProps) => {
   const { text, tags, period, role, team, links } = overview;
   return (
     <div className="flex flex-col md:flex-row gap-6 py-16 border-t border-gray-200">
@@ -19,22 +38,22 @@ const ProjectOverview = ({ overview, id }) => {
 
       <div className="grid grid-cols-2 gap-x-2 gap-y-6 w-full md:w-[52.18%] shrink-0">
         <div className="flex flex-col gap-1">
-          <InfomationTitle text="期間" />
+          <InformationTitle text="期間" />
           <span>{period ? period : ""}</span>
         </div>
 
         <div className="flex flex-col gap-1">
-          <InfomationTitle text="役割" />
+          <InformationTitle text="役割" />
           <span>{role ? role : ""}</span>
         </div>
 
         <div className="flex flex-col gap-1">
-          <InfomationTitle text="チーム規模" />
+          <InformationTitle text="チーム規模" />
           <span>{team ? team : ""}</span>
         </div>
 
         <div className="flex flex-col gap-1">
-          <InfomationTitle text="リンク" />
+          <InformationTitle text="リンク" />
           <div className="flex flex-col gap-2">
             {links.sourceUrl && (
               <a
