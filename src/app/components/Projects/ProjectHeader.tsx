1839abfdf63f538f7fa5949c4253250625c8d2aa
miura takayuki <6015abc@gmail.com>
2024-12-18 02:03:59 +0900

    パディングを追加

--- src/app/components/Projects/ProjectHeader.tsx
+++ src/app/components/Projects/ProjectHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectHeader = ({ title, description, image }) => {
   return (
-    <div className="flex flex-col md:flex-row-reverse gap-6">
+    <div className="flex flex-col md:flex-row-reverse gap-6 pb-16">
       <figure className="w-full h-full aspect-video md:w-[52.18%] shrink-0 border border-gray-200 rounded-xl">
         <img
           className="w-full h-full object-cover rounded-xl"
