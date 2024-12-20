1fc7a1132ece7c5623c685bcdf0c0875720debba
miura takayuki <6015abc@gmail.com>
2024-12-20 22:46:30 +0900

    Headerをページ遷移できるように調整

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import { getAssetPath } from "./Utils/Process";
 
 export default function Header() {
   return (
     <header className="fixed z-50 top-0 w-full bg-white/80 backdrop-blur-sm border-b border-gray-200">
       <div className="max-w-6xl h-16 flex justify-between items-center px-4 mx-auto">
-        <Link href="/" legacyBehavior>
-          <a className="font-semibold text-lg">Miura</a>
+        <Link href={getAssetPath("/")}>
+          <span className="font-semibold text-lg">Miura</span>
         </Link>
         <nav>
           <ul className="flex space-x-8 text-sm">
@@ -19,20 +20,20 @@ export default function Header() {
               </a>
             </li> */}
             <li>
-              <a
-                href="#projects"
+              <Link
+                href={getAssetPath("/#projects")}
                 className="transition-all text-gray-600 hover:text-gray-900 border-b border-transparent hover:border-gray-900"
               >
                 Projects
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#skills"
+              <Link
+                href={getAssetPath("/#skills")}
                 className="transition-all text-gray-600 hover:text-gray-900 border-b border-transparent hover:border-gray-900"
               >
                 Skills
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
